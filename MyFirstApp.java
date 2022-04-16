c9b660cb6bb0af43fb25fb3fa343fe1c66ed5b17
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-04-16 14:40:10 +0900

    Add files via upload

---
+++ MyFirstApp.java
@@ -0,0 +1,36 @@
+package createnewproject;
+
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		//값 == 데이터 == 자료
+		//자료형(데이터타입)
+		System.out.println(100);//정수(int)
+		System.out.println(100.0);//실수(double)
+		System.out.println("안녕하세요");
+		System.out.println("100");//문자열(string)
+
+		//100을 정수로 3번 출력한다. 
+		//변수선언
+		//f11 디버
+		int num1;
+		int num2;
+		
+		
+		num1 = 10;
+		num2 = 20;
+		
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	
+		int num3 = 30; //한줄로 표현하기 
+		String str1 = "hello";
+		double dnum = 3.14;
+
+		
+	}		
+
+
+}
---
+++ test1.java
@@ -0,0 +1,44 @@
+package createnewproject;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+	int num = 100;
+	System.out.println(num); // 출력 : 100
+	
+	double dnum = 3.14;
+	System.out.println(dnum); // 출력 : 3.14
+
+	String str1 = "안녕하세요";
+	System.out.println(str1); // 출력 : 안녕하세요
+
+	int num2 = 200;
+	System.out.println(num2); // 출력 : 200
+
+	double dnum2 = 10.5;
+	System.out.println(dnum2); // 출력 : 10.5
+
+	
+	String str2 = "반갑습니다. ";
+	System.out.println(str2); // 출력 : 반갑습니다.
+	
+	
+	  	int a = 5;
+	    int b = 10;
+
+	    System.out.println("a : " + a);
+	    System.out.println("b : " + b);
+	   
+	    // 여기서 부터 
+	    
+	    // 여기까지 수정 가능
+	    
+	    System.out.println("a : " + a);
+	    // 출력 : a : 10
+	    System.out.println("b : " + b);
+	    // 출력 :  b : 5
+
+	}
+
+}
