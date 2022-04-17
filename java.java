9aa29fd33341abf307cb9f279ee2a60678e286fe
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-04-17 13:01:08 +0900

    Add files via upload

---
+++ java.java
@@ -0,0 +1,58 @@
+package createnewproject;
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		1. 맑음 2. 비
+//		int weather = 1;
+////		system.out.println(weather);
+
+		//		양자택일
+//		if (weather == 1) {
+//			System.out.println("창문을 열어라");
+//		}
+//		else {
+//			System.out.println("창문을 닫아라");	
+//		}
+		
+		//할인대상, 성별, 동전의 앞뒤, 미성년자
+//		int sex = 1; //1이면 남자 2이면 여자 
+//		if(sex==1) {
+//			System.out.println("남자입니다.");
+//		}
+//		else {
+//			System.out.println("여자입니다.");
+//		}
+		
+		//삼자택일
+		
+//		int crossroad = 1; //1이면 좌회전이고 2이면 우회전 3이면 직진 
+		
+//		if(crossroad == 2) {
+//			System.out.println("직진을 합니다.");
+//		}
+//		else if(crossroad == 3) {
+//				System.out.println("좌회전을 합니다.");
+//		}
+//		else {
+//				System.out.println("우회전을 합니다.");			
+//		}	
+		
+		
+		int age = 40;
+		//나이에 따른 학적구분
+		// 10이면 청소년 20이면 대학생 30 이면 직장인 
+		
+		if(age >= 10 && age <=20 ) {
+			System.out.println("청소년입니다.");
+		}
+		else if(age >= 20 && age <= 30) {
+			System.out.println("직장인입니다.");
+		}
+		else {
+			System.out.println("주부입니다.");		
+
+		}
+					
+	}
+}
