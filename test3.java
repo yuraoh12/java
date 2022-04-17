c0c311de3f978662361be2071ec54dcc6b52441d
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-04-17 13:54:50 +0900

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,53 @@
+package createnewproject;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	반복문
+// 명령문 n번 반복해라		
+// 반복문 ==> 반복횟수를 세서 내가 원하는 횟수에서 멈춘다. 
+		//for(시작;끝;증가량)
+		
+//		for(int i = 0; i < 10; i = i+1) {
+//			System.out.println("hello");
+			
+//		}
+		
+//		for(int n =100; n<=200; n = n+1) {
+//			System.out.println(n);
+//		}
+
+		//100~1000 사이의 짝수 출력
+//		for(int i =0; i<=1000;i = i+2) {
+//		System.out.println(10+20);
+
+//		}
+
+//			}
+//		}
+//		// + : 더하기 
+//		System.out.println(100 + 20);
+//		// - : 빼기 
+//		System.out.println(100 - 20);
+//		// * : 곱하기 
+//		System.out.println(100 * 20);
+//		// % : 나누기 몫
+//		System.out.println(100 / 20);
+//		//  : 나누기 나머지
+//		System.out.println(100 % 30);
+//		
+//		//2로 나누어서 떨어지면 -> 2의배수 
+		
+		for(int i=100; i<=1000; i = i+1) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+	}
+			else {
+				System.out.println("홀수입니다.");
+			}
+			
+				}
+			}
+
+}
