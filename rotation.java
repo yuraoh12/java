4192f726eb74afdebcb574d99a375323ac048c01
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-04-24 14:50:03 +0900

    Add files via upload

---
+++ rotation.java
@@ -0,0 +1,36 @@
+package xacxsdas;
+
+class rotation {
+
+public static void gugu(int dan) {
+	
+
+	for (int i = 1; i < 10; i++) {
+		System.out.printf("%d X %d = %d\n", dan, i, dan * i);
+
+	}
+	
+}	
+public static void gugu2(int dan2) {
+	
+
+
+	for (int i = 1; i < 10; i++) {
+		System.out.printf("%d X %d = %d\n", dan2, i, dan2 * i);
+	}
+
+}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		gugu(2);
+		gugu(3);
+		gugu(4);
+
+		
+	}
+			
+	
+}
