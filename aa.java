d56cc78d3290211197eefa00ed410da8085e5c5b
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-04-24 13:54:28 +0900

    Add files via upload

---
+++ aa.java
@@ -0,0 +1,56 @@
+package xacxsdas;
+
+public class aa {
+	// 정의 
+	public static void test() {
+		
+		System.out.println("알바1:");
+		System.out.println("오하요");
+		System.out.println("곤니찌와");
+		System.out.println("곰방와");
+
+		
+	}
+	
+	
+	public static void test2() {
+		System.out.println("알바2:");
+		System.out.println("hello");
+		System.out.println("nice to meet you");
+		System.out.println("bye");
+
+		
+	}
+	
+	public static void test3() {
+		System.out.println("알바3:");
+		System.out.println("hello");
+		System.out.println("nice to meet you");
+		System.out.println("bye");
+
+		
+	}
+//main 함수 ==> 절차를 저장하고 연산을 수행하는 주체 
+// 함수 사용하면 ==> 일이 분담이 돼서 책임 소재가  명확하다. 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+//		test();
+	//	System.out.println("사장: 코드1");
+		
+//		test2();
+//		System.out.println("사장: 코드2");
+		
+		test3();
+		System.out.println("사장: 코드3");
+//		
+	}
+
+	
+	
+	
+	
+}
---
+++ yes24.java
@@ -0,0 +1,43 @@
+package xacxsdas;
+
+public class yes24 {
+
+	
+    public static void hi1() {
+        System.out.println("저는 차태진입니다.");
+    }
+
+    public static void hi2() {
+        System.out.println("안녕하세요.");
+    }
+
+    public static void hi3() {
+        System.out.println("프로그램을 만들어보아요.");
+    }
+
+    public static void hi4() {
+        System.out.println("자바를 통해");
+    }
+
+    
+	public static void main(String[] args) {
+
+		
+		
+		hi1(); 
+		System.out.println("저는 차태진입니다.");
+	
+		hi2();
+        System.out.println("안녕하세요.");
+
+        hi3();
+        System.out.println("프로그램을 만들어보아요.");
+
+        hi4();
+        
+        System.out.println("자바를 통해");
+
+		
+		
+	}
+}
