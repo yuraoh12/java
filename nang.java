2a142747798035ab03bba04e1cfc5560e1f10edd
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-04-30 13:29:58 +0900

    Add files via upload

---
+++ nang.java
@@ -0,0 +1,22 @@
+package xacxsdas;
+
+public class nang {
+
+	
+	public static void test(int num) {
+		System.out.println(num*num);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+
+		test(5);
+
+		test(10);
+		
+		
+		
+	}
+
+}
