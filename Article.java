d099d551edc0788d1339fab33e5f03a843eb223c
yuraoh12 <102283560+yuraoh12@users.noreply.github.com>
2022-05-15 14:50:13 +0900

    데이터 구조 로직화

---
+++ Article.java
@@ -0,0 +1,12 @@
+package aaaaa;
+
+public class Article {
+
+	String title;
+	String body;
+	
+	public Article(String title, String body) {
+		
+	}
+
+}
---
+++ Board.java
@@ -0,0 +1,10 @@
+package aaaaa;
+
+public class Board {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
